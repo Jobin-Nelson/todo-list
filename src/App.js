229d53f70a3523ce9747859b0241ac976db4d5bb
Jobin Nelson <jobinnelson369@gmail.com>
2023-06-10 16:58:13 +0530

    add Todo components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
-import { TodoWrapper } from './components/TodoWrapper';
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
 
-function App() {
+function App(props) {
+  const taskList = props.tasks?.map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+    />
+  ))
   return (
-    <div className="App">
-      <TodoWrapper />
+    <div className="todoapp stack-large">
+      <h1>Todo List</h1>
+      <Form />
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
     </div>
   );
 }
-
 export default App;
+
